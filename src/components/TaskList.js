b599255fa1d4dae6770b2b6d11943875ae90b3b8
Jeremy Cook <jcook61592@gmail.com>
2021-06-11 17:26:46 -0400

    Jeremy commit

---
+++ src/components/TaskList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Task from './Task';
+
+const TaskList = ({ tasks }) => {
+  return (
+    <div className='tasklist-container'>
+      {tasks.map((singleTask) => {
+        return <Task singleTask={singleTask} />;
+      })}
+    </div>
+  );
+};
+
+export default TaskList;
